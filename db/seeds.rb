4cb3f736b1a8aad7d2c63a4ff79157199ea377ff
discocisco <f.andresort@gmail.com>
2019-03-11 14:17:55 -0400

    Add sample movie data seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Movie.delete_all
+Movie.create(name: 'Captain Marvel', release_date: '2019-03-08', poster: 'https://i.redd.it/byqexntpuyk21.jpg')
+Movie.create(name: 'Watchmen', release_date: '2009-03-06', poster: 'https://i.redd.it/ob7kfa6wd7l21.jpg')
+Movie.create(name: 'The Dark Knight', release_date: '2008-07-18', poster: 'https://i.redd.it/exzbdpka26l21.jpg')
+Movie.create(name: 'Deadpool', release_date: '2016-02-12', poster: 'https://i.redd.it/26yrbxeodsk21.jpg')
+Movie.create(name: 'Black Panther', release_date: '2018-02-16', poster: 'https://i.redd.it/4y99cw69okk21.jpg')
+Movie.create(name: 'Trainspotting', release_date: '1996-07-19', poster: 'https://i.redd.it/kytmk2jrpkk21.jpg')
+Movie.create(name: 'The Fifth Element', release_date: '1997-05-09', poster: 'https://i.redd.it/05c8mt66x7k21.jpg')
+Movie.create(name: 'Hereditary', release_date: '2018-06-08', poster: 'https://i.redd.it/703gewzdj5k21.jpg')
+
+puts 'Success: Movie data loaded'
