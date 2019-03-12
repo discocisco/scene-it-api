a4a1420cd65f5da07ebe9777e56f7dd99f3dc834
discocisco <f.andresort@gmail.com>
2019-03-12 19:24:56 -0400

    Add favorites curl-scripts
    - Add methods for 'GET', 'SHOW', 'POST', 'PATCH', 'DESTROY'

---
+++ curl-scripts/favorites/update.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+curl "http://localhost:4741/favorites/${ID}" \
+  --include \
+  --request PATCH \
+  --header "Content-Type: application/json" \
+  --header "Authorization: Token token=${TOKEN}" \
+  --data '{
+    "favorite": {
+      "movie_id": "'"${MOVIE_ID}"'"
+    }
+  }'
+
+echo
